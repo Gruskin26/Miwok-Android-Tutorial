b9bd67bd3ff5701af72f09c6c4281064faf4b1cd
Michael <Mag5856@rit.edu>
2018-01-29 21:16:33 -0500

    The course I am following has had me transition from on click events for each menu option to the use of an OnClickListener for each, which has now been implemented. At this point, I have completed the first lesson (of 5) of the multi-screen Android application module of the Android Basics class.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,6 +19,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +31,52 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+        TextView family = (TextView) findViewById(R.id.family);
+        TextView colors = (TextView) findViewById(R.id.colors);
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        // Set up onClickListeners in order to open up the appropriate view for each
+        // menu item
+
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
     }
 
     public void openNumbersList(View view){
@@ -35,18 +84,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void openPhrasesList(View view){
-        Intent i = new Intent(this, PhrasesActivity.class);
-        startActivity(i);
-    }
 
-    public void openColorsList(View view){
-        Intent i = new Intent(this, ColorsActivity.class);
-        startActivity(i);
-    }
-
-    public void openFamilyList(View view){
-        Intent i = new Intent(this, FamilyActivity.class);
-        startActivity(i);
-    }
 }
